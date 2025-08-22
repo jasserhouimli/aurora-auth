b57a9b2e7f03559a43d6e7ad7cc4a025a87cdb57
Swag <houimlijasser9@gmail.com>
2025-08-22 13:23:02 +0100

    Add tower-http dependency and update server startup message

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ async fn main() {
     let api_path = format!("/api/{}", env::var("APIVERSION").unwrap_or("v1".to_string()));
     let app = Router::new().nest(&api_path, route::get_routers().with_state(app_state));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Server is ready at http://localhost:3000");
+    let addr = listener.local_addr().unwrap();
+    println!("Listening on {}", addr);
     axum::serve(listener, app).await.unwrap();
 }
